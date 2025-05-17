8dc4c03ed908d07c05e5e2dfd618bce3c5cbcd1b
Iswarya V <161108333+Iswaryavishwanathan@users.noreply.github.com>
2025-05-17 15:22:56 +0530

    Delete DataMillR-main/DataMillR-main directory

--- DataMillR-main/DataMillR-main/README.md
+++
@@ -1,1 +0,0 @@
-"# DataMillR" 
--- DataMillR-main/DataMillR-main/modbusserverapp/src/main/java/com/example/modbusapplication/Repository/DeviceMappingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.modbusapplication.Repository;
-
-import com.example.modbusapplication.Entity.DeviceMapping;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeviceMappingRepository extends JpaRepository<DeviceMapping, Long> {
-}
